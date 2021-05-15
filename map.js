fb668052e9636fdfe6384899801aafc10951f6a8
Faruq Hossen <30349832+faruqhossen30@users.noreply.github.com>
2021-05-15 22:41:19 +0600

    Mapbox map api
    Map Like goole

---
+++ map.js
@@ -0,0 +1,7 @@
+  mapboxgl.accessToken = 'pk.eyJ1IjoiZmFydXFob3NzZW4zMCIsImEiOiJja284cWs1anUxeWU4Mm5scmxwYXo2bTg3In0.2HIV2uP72bXWQCstp6-LDw';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    
+  });
+  
